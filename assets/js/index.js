36d4a5c62a9ecd86b556a91e8be7b5ae00d64df9
NetherDragon99 <netherdragon114@gmail.com>
2026-07-05 05:50:45 +0300

    adding js scroll animation and style on navigate between pages remember last opened page

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+
+// page scroll navigation style function
+const pagesFooter = document.querySelectorAll('footer>#footerContainer>a:not(:nth-child(3))');
+const selectedBar = document.querySelector('footer> #selectedArea');
+const footerContainer = document.getElementsByTagName('footer')[0];
+
+pagesFooter.forEach(v => v.addEventListener('click', () => {
+  pagesFooter.forEach(value => value.classList.remove('selected'));
+
+  v.classList.add('selected');
+  selectedBar.style.left = `${v.getBoundingClientRect().left - footerContainer.getBoundingClientRect().left}px`;
+  selectedBar.style.width = `${v.getBoundingClientRect().width}px`;
+
+  localStorage.setItem('lastOpenedPage', JSON.stringify(location.hash))
+
+  console.log(v.getBoundingClientRect().left, footerContainer.getBoundingClientRect().left);
+}))
+
+window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    let hash = location.hash
+    if (!hash) {
+      localStorage.setItem('lastOpenedPage', '#homePage');
+      hash = '#homePage';
+    }
+    document.querySelector(`body>section#MainContainer ${hash}`).scrollIntoView({
+      behavior: 'smooth'
+    })
+
+    pagesFooter.forEach(value => value.classList.remove('selected'));
+
+    document.querySelector(`${hash}Footer`).classList.add('selected');
+
+    selectedBar.style.left = `${document.querySelector(`${hash}Footer`).getBoundingClientRect().left - footerContainer.getBoundingClientRect().left}px`;
+    selectedBar.style.width = `${document.querySelector(`${hash}Footer`).getBoundingClientRect().width}px`;
+    selectedBar.style.borderBottom = 'rgb(199, 255, 255) solid 2px'
+  }, 500);
+})
